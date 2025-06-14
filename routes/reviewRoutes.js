8054df5341fcffa8752df9d9f12edf260fde6183
Prabodh Raj U R <prabodhrajmow@gmail.com>
2025-06-14 20:58:12 +0530

    small changes in review router

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const router = express.Router();
+
+const {
+  createReview,
+  getAllReviews,
+  updateReview,
+  deleteReview,
+} = require('../controllers/reviewController');
+const { protect } = require('../middleware/authMiddleware');
+
+// Routes
+router.post('/products/:productId/reviews', protect, createReview);
+router.get('/reviews', getAllReviews);
+router.patch('/reviews/:id', protect, updateReview);
+router.delete('/reviews/:id', protect, deleteReview);
+
+module.exports = router;
